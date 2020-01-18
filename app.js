92210d85c68aa5bde28f12f0461e4d71d6de756e
André <andredias.us@gmail.com>
2020-01-18 03:35:23 -0300

    Tentativa de usar mongoose com o banco

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+
 const port = process.env.PORT || 3000;
 //Listening to the server
 app.listen(port);
 
-//Middlewares
-app.use('/', () => {
-    console.log('Middleware at home!');
+//Connecting to the database
+const MONGODB_URL = process.env.MONGODB_URI || '';
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true}, () => {
+    console.log('Connected to the database!');
 });
 
+
+//Middlewares
+// app.use('/', () => {
+//     console.log('Middleware at home!');
+// });
 
 
 //Routes
