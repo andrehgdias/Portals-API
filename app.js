c404f9c2f5a5fbba95d1fca5bb26bff87e567137
André <andredias.us@gmail.com>
2020-01-18 03:08:48 -0300

    Middleware at home

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const port = process.env.PORT || 3000;
 //Listening to the server
 app.listen(port);
 
+//Middlewares
+app.use('/', () => {
+    console.log('Middleware at home!');
+});
+
+
+
 //Routes
 app.get('/', (req, res) => {
     res.send('We are online baby!')
