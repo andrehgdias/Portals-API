bf0c3f7d5651b265581f0428d98c5ef76387698e
André <andredias.us@gmail.com>
2020-01-18 12:12:56 -0300

    Models

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const postsRoute = require('./routes/posts');
 
 const port = process.env.PORT || 3000;
+
 //Listening to the server
 app.listen(port);
 
@@ -14,10 +15,8 @@ mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true}
     console.log('Connected to the database!');
 });
 
-
 //Middlewares
 app.use('/posts', postsRoute);
-
 
 //Routes
 app.get('/', (req, res) => {
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const router = express.Router();
+const Posts = require('../models/Posts');
 
 router.get('/', (req, res) => {
     res.send('We are on posts baby!')
@@ -8,6 +8,11 @@ router.get('/', (req, res) => {
 
 router.get('/status', (req, res) => {
     res.send('We are on posts STATUS baby!')
+});
+
+
+router.post('/', (req, res) => {
+    console.log(req.body);
 });
 
 module.exports = router;
