cea0b195dfbf3fef219a0f4d0247d249ad1e121a
André <andredias.us@gmail.com>
2020-01-18 03:53:30 -0300

    Posts route especif

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('We os posts baby!')
+    res.send('We are on posts baby!')
+});
+
+router.get('/status', (req, res) => {
+    res.send('We are on posts STATUS baby!')
 });
 
 module.exports = router;
