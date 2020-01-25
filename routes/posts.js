eb7136c4da29220039f2280029f9d963cce90e3c
André Dias <andredias.us@gmail.com>
2020-01-25 12:10:54 -0300

    Trying using find

--- routes/posts.js
+++ routes/posts.js
@@ -27,4 +27,20 @@ router.post("/", (req, res) => {
     });
 });
 
+router.post("/find", (req, res) => {
+  const post = new Posts({
+    title: req.body.title
+  });
+
+  post
+    .find({ title: req.title })
+    .then(data => {
+      // Create a promise to respond to our request after saving our req.body on the db
+      res.json(data);
+    })
+    .catch(err => {
+      res.json({ msg: "Erro ao tentar encontrar os dados: ", err });
+    });
+});
+
 module.exports = router;
