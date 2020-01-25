2a7d0ac3c2f93eb2899d06f023fe3f8b979654fe
André Dias <andredias.us@gmail.com>
2020-01-25 12:16:04 -0300

    Changing the way our post route is written

--- routes/posts.js
+++ routes/posts.js
@@ -10,37 +10,18 @@ router.get("/status", (req, res) => {
   res.send("We are on posts STATUS baby!");
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const post = new Posts({
     title: req.body.title,
     description: req.body.description
   });
-
-  post
-    .save()
-    .then(data => {
-      // Create a promise to respond to our request after saving our req.body on the db
-      res.json(data);
-    })
-    .catch(err => {
-      res.json({ msg: "Erro ao tentar salvar os dados: ", err });
-    });
-});
-
-router.post("/find", (req, res) => {
-  const post = new Posts({
-    title: req.body.title
-  });
-
-  post
-    .find({ title: req.title })
-    .then(data => {
-      // Create a promise to respond to our request after saving our req.body on the db
-      res.json(data);
-    })
-    .catch(err => {
-      res.json({ msg: "Erro ao tentar encontrar os dados: ", err });
-    });
+  
+  try {
+    const savedPost = post.save();
+    res.json(savedPost);
+  } catch (err) {
+    res.json({ msg: "Erro ao tentar salvar os dados: ", err });
+  }
 });
 
 module.exports = router;
