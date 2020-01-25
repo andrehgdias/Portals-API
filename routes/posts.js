b882c2669faea8d69da37a48335cb89b647d8f58
André Dias <andredias.us@gmail.com>
2020-01-25 12:37:06 -0300

    Added find all route

--- routes/posts.js
+++ routes/posts.js
@@ -2,20 +2,25 @@ const express = require("express");
 const router = express.Router();
 const Posts = require("../models/Posts");
 
-router.get("/", (req, res) => {
-  res.send("We are on posts baby!");
+router.get("/all", async (req, res) => {
+  try {
+    const posts = await Posts.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ msg: "Erro ao tentar buscar os dados: ", err });
+  }
 });
 
 router.get("/status", (req, res) => {
   res.send("We are on posts STATUS baby!");
 });
 
-router.post("/", async (req, res) => {
+router.post("/new", async (req, res) => {
   const post = new Posts({
     title: req.body.title,
     description: req.body.description
   });
-  
+
   try {
     const savedPost = await post.save();
     res.json(savedPost);
